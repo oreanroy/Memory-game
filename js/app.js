5658a32c02e959ab88b0950ae9cbd3d26dd5bd2f
oreanroy <oreanroy@gmail.com>
2018-07-03 00:15:17 +0530

    add ellaborative comments

--- js/app.js
+++ js/app.js
@@ -5,17 +5,18 @@ deckArea = document.querySelector('.deck');
 let count = 0;
 let moves = 0;
 let oldCard = [];
-var countFlag = 0;
-var oldClass = "";
-var resetButton = document.querySelector('.restart');
-var movesindicator = document.querySelector('.moves');
-var movesCount = 1;
-var star = document.querySelector('.stars');
+let countFlag = 0;
+let oldClass = "";
+let resetButton = document.querySelector('.restart');
+let movesindicator = document.querySelector('.moves');
+let movesCount = 1;
+let star = document.querySelector('.stars');
 let timeCount = 0;
-var time = document.querySelector('.timer');
-//function to arrange the board 
+let time = document.querySelector('.timer');
+
+//function to arrange the board
 function setBoard(){
-shuffleArray = shuffle(cardClass);
+shuffleArray = shuffle(cardClass); //get the shuffled array
 for(let i = 0;i<cardClass.length; i++){
     listItem = document.createElement('li');
     listItem.className = "card";
@@ -25,13 +26,14 @@ for(let i = 0;i<cardClass.length; i++){
     icon = document.createElement('i');
     icon.className = classType;
     icon.classList.add("fa");
-    listItem.appendChild(icon); 
+    listItem.appendChild(icon);
     deckArea.appendChild(listItem);
 };
 play();
 };
-setBoard();
 
+
+//Function to start the play
 var firstClick = true;
 function play(){
 deckArea.addEventListener('click', function(e) {
@@ -51,30 +53,28 @@ if(e.target.id!=oldCard.id && classNam === oldClass && countFlag==2){
 }else{
         if(e.target.id!=oldCard.id  && countFlag==2){
           open(e);
-            
         }
     }
-        if(e.target.id!=oldCard.id && countFlag==1){
-            oldCard.push(e.target);
-            oldClass = e.target.childNodes[0].className;
-        }
+    if(e.target.id!=oldCard.id && countFlag==1){
+        oldCard.push(e.target);
+        oldClass = e.target.childNodes[0].className;
+    }
 });
 };
 
 //function if two cards do not match
 function open(e){
-setTimeout(function() { e.target.setAttribute('class', 'card'); oldCard[0].setAttribute('class', 'card');
-countFlag=0; 
-oldCard =   [];
-oldClass =  "";}, 400);
-
+  setTimeout(function() { e.target.setAttribute('class', 'card'); oldCard[0].setAttribute('class', 'card');
+    countFlag=0;
+    oldCard =   [];
+    oldClass =  "";}, 400);
 }
 // function to check if two functions match
 function match(e){
 e.target.setAttribute('class', 'card  match open');
     oldCard[0].setAttribute('class', 'card  match open');
-    cardOpen.push(oldClass); 
-    countFlag=0; 
+    cardOpen.push(oldClass);
+    countFlag=0;
     oldCard = [];
     oldClass = "";
     TestComplete(cardOpen);
@@ -82,7 +82,7 @@ e.target.setAttribute('class', 'card  match open');
 //reset button event listener
 
 resetButton.addEventListener('click', function(){
-reset();
+    reset();
 });
 
 
@@ -101,56 +101,55 @@ return array;
 }
 // function to test if the game is complete
 function TestComplete(open){
-if(open.length==8){
-message.innerHTML = `congragulation!! you have won the game in ${moves} moves`;
-modal.style.display = "block";
-}else
-return null;
+    if(open.length==8){
+        message.innerHTML = `congragulation!! you have won the game in ${moves} moves`;
+        modal.style.display = "block";
+    }else
+    return null;
 };
 //function to increse the moves pointer
 function moveIncrease(){
-movesindicator.innerHTML=`${moves}`;
-if(moves>=14*movesCount)  //to decrease the number of stars after every 28 moves,
-starDecrease();
+    movesindicator.innerHTML=`${moves}`;
+    if(moves>=14*movesCount)  //to decrease the number of stars after every 28 moves,
+        starDecrease();
 };
 
 //function to decrease the number of stars
 function starDecrease(){
-star.removeChild(star.childNodes[0]); 
-movesCount++;
+    star.removeChild(star.childNodes[0]);
+    movesCount++;
 };
+// function to add three starts on reset
 function addStar(){
-star.innerHTML="<li><i class='fa fa-star'></i></li><li><i class='fa fa-star'></i></li><li><i class='fa fa-star'></i></li>";
+    star.innerHTML="<li><i class='fa fa-star'></i></li><li><i class='fa fa-star'></i></li><li><i class='fa fa-star'></i></li>";
 }
 
 //timer function
 function timeClock(){
-setTimeout(function() {
-timeCount++;
-time.innerHTML = `${timeCount} secs`;
-if(cardOpen.length<8)
-timeClock();
-}, 1000);
+    setTimeout(function() {
+    timeCount++;
+    time.innerHTML = `${timeCount} secs`;
+    if(cardOpen.length<8)
+        timeClock();
+    }, 1000);
 }
 //function to reset
 function reset(){
- cardOpen = [];
- cardpos = [];
- count = 0;
- moves = 0;
- oldCard = [];
- countFlag = 0;
- oldClass = "";
-deckArea.innerHTML = "";
-movesCount=1;
-timeCount = 0;
-firstClick = true;
-time.innerHTML = `0 secs`;
-movesindicator.innerHTML=`0`;
-addStar();
-setBoard();
-
-
+    cardOpen = [];
+    cardpos = [];
+    count = 0;
+    moves = 0;
+    oldCard = [];
+    countFlag = 0;
+    oldClass = "";
+    deckArea.innerHTML = ""; //remove older cards
+    movesCount=1;
+    timeCount = 0;
+    firstClick = true;
+    time.innerHTML = `0 secs`;// set the clock to zero
+    movesindicator.innerHTML=`0`;// set the moves count to zero
+    addStar();//set he stars to three
+    setBoard();
 }
 
 // modal driving Js
@@ -161,14 +160,15 @@ let message = document.getElementById("score");
 btn.onclick = function(){
 modal.style.display = "none";
 reset();
-}
+};
+// if cliked on cross close the modal
 span.onclick = function(){
-modal.style.display = "none";
-}
-window.onclick = function(event){
-if(event.target == modal){
     modal.style.display = "none";
-}
-}
-
-
+};
+//if clicked outside modal close the modal
+window.onclick = function(event){
+    if(event.target == modal){
+        modal.style.display = "none";
+    }
+};
+setBoard(); // start the game by setting the board.
